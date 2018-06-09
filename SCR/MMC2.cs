c7515c12884ad86de2680633dac0793c640f21ca
Nhoppasit S <39229469+nhoppasit@users.noreply.github.com>
2018-06-09 17:54:06 +0700

    ลิงค์ wpf mvc

---
+++ SCR/MMC2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quintessence.MMC2Model
+{
+    public class MMC2
+    {
+        /* ----------------------------------------------------------  
+         * พอร์ต
+         * ----------------------------------------------------------  */
+        private string _SerialPortName; public string SerialPortName { get; set; }
+
+        /* ----------------------------------------------------------  
+         * ข้อมูลตำแหน่ง
+         * ----------------------------------------------------------  */
+        private int _ActualXStep; public int ActualXStep { get; set; }
+        private int _ActualYStep; public int ActualYStep { get; set; }
+        private bool _IsReady; public bool IsReady { get; set; }
+
+        /* ----------------------------------------------------------  
+         * การเคลื่อนที่แบบสัมพัทธ์
+         * ----------------------------------------------------------  */
+        private int _XRelativeStep; public string XRelativeStep { get; set; }
+        private int _YRelativeStep; public string YRelativeStep { get; set; }
+
+    }
+}
---
+++ SCR/MMC2Control.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quintessence.MMC2Model;
+
+namespace Quintessence.MMC2Control
+{
+    public class MMC2Control
+    {
+        private MMC2 obj = new MMC2();
+        public string SerialPortName
+        {
+            get { return obj.SerialPortName; }
+            set { obj.SerialPortName = value; }
+        }
+        public string ActualXStep
+        {
+            get { return obj.ActualXStep.ToString("0"); }
+            set { obj.ActualXStep = Convert.ToInt32(value); }
+        }
+        public string ActualYStep
+        {
+            get { return obj.ActualYStep.ToString("0"); }
+            set { obj.ActualYStep = Convert.ToInt32(value); }
+        }
+
+    }
+}
