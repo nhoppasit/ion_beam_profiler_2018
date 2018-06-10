21c7b98265e8f823a2aca1de9853299c81fc417e
Nhoppasit <dev.theprogram@gmail.com>
2018-06-10 20:40:55 +0700

    Creating of Gpib34401aViewModel

---
+++ SCR/WPF Winapp/Quintessence.Meter/GPIB34401A/GPIB34401AViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Quintessence.Meter.GPIB34401A
+{
+    public class GPIB34401AViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
