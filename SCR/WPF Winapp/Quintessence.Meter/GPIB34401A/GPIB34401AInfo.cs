5bb8b88de8c13254285d7a8db7479edea46f72f3
Nhoppasit <dev.theprogram@gmail.com>
2018-06-10 22:43:16 +0700

    เปลี่ยนแนวทางในการทำ relaycommand

--- SCR/WPF Winapp/Quintessence.Meter/GPIB34401A/GPIB34401AInfo.cs
+++ SCR/WPF Winapp/Quintessence.Meter/GPIB34401A/GPIB34401AInfo.cs
@@ -1,22 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
-namespace Quintessence.Meter.GPIB34401A
+namespace Quintessence.Meter.Gpib34401a
 {
-    public class GPIB34401AInfo
+    public class Gpib34401aInfo : INotifyPropertyChanged
     {
+        #region Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        public Gpib34401aInfo(int BoardNumber, int address)
+        {
+
+        }
+
+        /* ----------------------------------------------------------  
+         * Gpib Name
+         * ----------------------------------------------------------  */
+        private string _GpibInterfaceId;
+        public string GpibInterfaceId
+        {
+            get
+            {
+                return _GpibInterfaceId;
+            }
+        }
+        private int _GpibBoardNumber;
+        public int GpibBoardNumber
+        {
+            get { return _GpibBoardNumber; }
+            set
+            {
+                _GpibBoardNumber = value;
+                _GpibInterfaceId = "GPIB" + _GpibBoardNumber.ToString();
+                OnPropertyChanged("GpibBoardNumber");
+                OnPropertyChanged("GpibInterfaceId");
+            }
+        }
+        private string _GpibAddress;
+        public string GpibAddress
+        {
+            get { return _GpibAddress; }
+            set
+            {
+                _GpibAddress = value;
+                OnPropertyChanged("GpibAddress");
+            }
+        }
+
         /* ----------------------------------------------------------  
          * Measured Value
          * ----------------------------------------------------------  */
-        private float _Current; public float Current { get { return _Current; } set { _Current = value; } }
-        private float _Voltage; public float Voltage { get { return _Voltage; } set { _Voltage = value; } }
+        private float _Current; public float Current { get { return _Current; } set { _Current = value; OnPropertyChanged("Current"); } }
+        private float _Voltage; public float Voltage { get { return _Voltage; } set { _Voltage = value; OnPropertyChanged("Voltage"); } }
 
         /* ----------------------------------------------------------  
          * Read Interval
          * ----------------------------------------------------------  */
-        private int _ReadIntervalMillisecond; public int ReadIntervalMillisecond { get { return _ReadIntervalMillisecond; } set { _ReadIntervalMillisecond = value; } }
-                
+        private int _ReadIntervalMillisecond; public int ReadIntervalMillisecond { get { return _ReadIntervalMillisecond; } set { _ReadIntervalMillisecond = value; OnPropertyChanged("ReadIntervalMillisecond"); } }
+
     }
+
+
+
+
 }
--- SCR/WPF Winapp/Quintessence.Meter/GPIB34401A/GPIB34401AViewModel.cs
+++ SCR/WPF Winapp/Quintessence.Meter/GPIB34401A/GPIB34401AViewModel.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel;
+using System.Collections.ObjectModel;
 
-namespace Quintessence.Meter.GPIB34401A
+namespace Quintessence.Meter.Gpib34401a
 {
-    public class GPIB34401AViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
+    public class Gpib34401aViewModel 
+    {        
+        // Model
+        Gpib34401aInfo _Gpig34401a;
+        public Gpib34401aInfo Gpig34401a { get { return _Gpig34401a; } set { _Gpig34401a = value; } }
+        
+        public Gpib34401aViewModel()
+        {
+            _Gpig34401a = new Gpib34401aInfo();
+        }
+
     }
 }
