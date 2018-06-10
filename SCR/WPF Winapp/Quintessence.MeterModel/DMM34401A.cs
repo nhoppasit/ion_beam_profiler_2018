6e2cd84f966e2933c8c15c3cb415f18847ff380d
Nhoppasit <dev.theprogram@gmail.com>
2018-06-10 12:44:47 +0700

    เพิ่ม dll ของ meter เป็น mvc

---
+++ SCR/WPF Winapp/Quintessence.MeterModel/DMM34401A.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quintessence.MeterModel
+{
+    public class DMM34401A
+    {
+        /* ----------------------------------------------------------  
+         * Measured Value
+         * ----------------------------------------------------------  */
+        private float _Current; public float Current { get { return _Current; } set { _Current = value; } }
+        private float _Voltage; public float Voltage { get { return _Voltage; } set { _Voltage = value; } }
+
+        /* ----------------------------------------------------------  
+         * Read Interval
+         * ----------------------------------------------------------  */
+        private int _ReadIntervalMillisecond; public int ReadIntervalMillisecond { get { return _ReadIntervalMillisecond; } set { _ReadIntervalMillisecond = value; } }
+
+        /* ----------------------------------------------------------  
+         * Read Interval
+         * ----------------------------------------------------------  */
+
+
+    }
+}
