04d020ecde57158d1c406598c7657600bfb29158
Nhoppasit <dev.theprogram@gmail.com>
2018-07-08 00:20:03 +0700

    Add new class of current grid model in the vm project

---
+++ SCR/IBP2018/Quintessence.Ibp2018/Model/CurrentGridModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastWpfGrid;
+
+namespace Quintessence.Ibp2018.Model
+{
+    public class CurrentGridModel : FastGridModelBase
+    {
+        private Dictionary<Tuple<int, int>, string> _editedCells = new Dictionary<Tuple<int, int>, string>();
+        private static string[] _columnBasicNames = new[] { "", "Value:", "Long column value:" };
+
+        public override int ColumnCount
+        {
+            get { return 70; }
+        }
+
+        public override int RowCount
+        {
+            get { return 1000; }
+        }
+
+        public override string GetCellText(int row, int column)
+        {
+            var key = Tuple.Create(row, column);
+            if (_editedCells.ContainsKey(key)) return _editedCells[key];
+
+            return "";
+        }
+
+        public override void SetCellText(int row, int column, string value)
+        {
+            var key = Tuple.Create(row, column);
+            _editedCells[key] = value;
+        }
+
+        public override void HandleSelectionCommand(IFastGridView view, string command)
+        {
+            MessageBox.Show(command);
+        }
+
+        public override IFastGridCell GetColumnHeader(IFastGridView view, int column)
+        {
+            var res = new FastGridCellImpl();
+            res.Blocks.Add(new FastGridBlockImpl
+            {
+                IsBold = true,
+                TextData = string.Format("X={0:0.00}", column * 0.02),
+            });
+            return res;
+        }
+
+        public override IFastGridCell GetRowHeader(IFastGridView view, int row)
+        {
+            var res = new FastGridCellImpl();
+            res.Blocks.Add(new FastGridBlockImpl
+            {
+                IsBold = true,
+                TextData = String.Format("Y={0:0.00}", row * 0.02)
+            });
+            return res;
+        }
+    }
+}
